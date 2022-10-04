db4897e893e2c1f2ae80148261af5a03b1cf57e9
danielstl <15856061+danielstl@users.noreply.github.com>
2022-10-04 17:15:07 +0100

    Updated build process to support split .js files

--- postbuild.js
+++ postbuild.js
@@ -1,11 +1,9 @@
-//&& mkdir build/dl/ && mv build/static/js/main*.js build/dl/dl.js && mv build/static/css/main*.css build/dl/dl.css
-
 const fs = require("fs");
 const path = require("path");
 
 fs.mkdirSync("./build/dl");
 
-const jsFile = path.join("./build/static/js/", fs.readdirSync("./build/static/js").find(file => file.endsWith(".js")));
+const jsFile = path.join("./build/static/js/", fs.readdirSync("./build/static/js").find(file => file.endsWith(".js") && !file.endsWith(".chunk.js")));
 const cssFile = path.join("./build/static/css/", fs.readdirSync("./build/static/css").find(file => file.endsWith(".css")));
 
 fs.renameSync(jsFile, "./build/dl/dl.js");
